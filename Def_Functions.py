f07f473633dd7c30adc8593f39d241e714f885ba
ShikharVeer10 <147176829+ShikharVeer10@users.noreply.github.com>
2024-01-02 11:14:43 +0530

    Add files via upload

---
+++ Def_Functions.py
@@ -0,0 +1,18 @@
+def my_function(): 
+    print("Hello")# Do this output
+    print("bye")#Then do this output
+    print("Hello Bye")
+    
+print(my_function()) # Finally print this output This output has nothing defined in the my_function so it is defined as none by default
+
+#Task: 
+# Create a function called Greet()
+# Write 3 print statements inside the function
+# Call the greet() function and run your code
+
+def greet():
+    print("Hello")
+    print("Bye")
+    print("Hello Bye")
+
+print(greet())
