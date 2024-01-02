4e6281484d23841288a775ae0573305ed2661182
ShikharVeer10 <147176829+ShikharVeer10@users.noreply.github.com>
2024-01-02 11:10:53 +0530

    Add files via upload

---
+++ Python Variables.py
@@ -0,0 +1,22 @@
+# Input Function(Length)
+name="Jack"
+print("Hello" + name)
+
+name="Angela"
+print(name)
+
+name=input("What is your name?")
+length=len(name) # Length function: TO determine the no of characters in the given string
+print(length)
+
+# Test Code
+a=input()
+b=input()
+#Create a 3rd variable to switch the variables
+c=a
+a=b
+b=c
+print("a:" +a)
+print("b:" +b)
+
+#Naming Variables
