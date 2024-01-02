6cbc26a90c61705e015369f4f679d49aaa86d0d9
ShikharVeer10 <147176829+ShikharVeer10@users.noreply.github.com>
2024-01-02 11:10:00 +0530

    Add files via upload

---
+++ Basic Functions.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("Day 1- What to Print")
+print("Hello World!/nHello World!")
+print("Hello"+"Angela")
+print("Hello" + "Angela")
+input("What is your name?")
+print("Hello"+input("What is your name?"))
